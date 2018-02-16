776bb51853c7fcb9c5264edad28f98a30d7a38b1
David Ketchum <dgketchum@gmail.com>
2018-02-16 11:31:25 -0700

    inital commit

--- NB_Ketchum.py
+++ NB_Ketchum.py
@@ -33,37 +33,47 @@ n = X.shape[1]
 N = target_arr.shape[0]
 m_test = X_test.shape[1]
 
-# feature select
+train_acc = 0.0
+# feature_select
+list_ = [x for x in range(1, n)]
+_list = list_
+for start in _list:
+    for stop in list_:
 
-theta = np.zeros((n, N))
+        n = abs(stop - start)
+        X_mod = X[:, start: stop]
+        theta = np.zeros((n, N))
+        if X_mod.shape[1] > 1:
+            for k in range(N):
+                theta[:, k] = np.sum(X_mod[y == k], axis=0) / len(X_mod[y == k])
 
-a = np.arange(0, n).reshape(8, 8)
-b = a[:, 2:7].reshape(40)
-X_mod = X[:, b]
-for k in range(N):
-    theta[:, k] = np.sum(X_mod[y == k], axis=0) / len(X_mod[y == k])
+            unique, counts = np.unique(y, return_counts=True)
+            priors = np.array([x / np.sum(counts) for x in counts])
 
-unique, counts = np.unique(y, return_counts=True)
-priors = np.array([x / np.sum(counts) for x in counts])
+            class_probs = np.zeros((m, N))
 
-class_probs = np.zeros((m, N))
+            for i, x in enumerate(X_mod):
+                for k in range(N):
+                    prior = priors[k]
+                    lklhd = np.prod(theta[:, k] ** x * (1 - theta[:, k]) ** (1 - x))
+                    pstrr_k = prior * lklhd
+                    class_probs[i, k] = pstrr_k
 
-for i, x in enumerate(X_mod):
-    for k in range(N):
-        prior = priors[k]
-        lklhd = np.sum(theta[:, k] * (1 - theta[:, k]) ** (1 - x))
-        pstrr_k = prior * lklhd
-        class_probs[i, k] = pstrr_k
-class_probs /= np.sum(class_probs, axis=1, keepdims=True)
+            class_probs /= np.sum(class_probs, axis=1, keepdims=True)
 
-y_pred_train = np.argmax(class_probs, axis=1)
+            y_pred_train = np.argmax(class_probs, axis=1)
 
-cm_train = confusion_matrix(y_pred_train, y)
+            cm_train = confusion_matrix(y_pred_train, y)
 
-# print(cm_train)
-# print(r)
-print('training accuracy: {}'.format(accuracy_score(y, y_pred_train)))
-# print('mean of features: {}'.format(np.mean(r)))
+            # print(cm_train)
+            # print(r)
+            trial_accuracy = accuracy_score(y, y_pred_train)
+            if trial_accuracy > train_acc:
+                print('training accuracy: {}'.format(trial_accuracy))
+                arr = X_mod[0, :]
+                train_acc = trial_accuracy
+                results = {'accuracy': train_acc, 'feature_array': arr}
 
+print(results)
 
 # ========================= EOF ====================================================================
